6f9a1715007c2d516bb64c0a63dc867af49e50c6
Vidra22 <145906805+Vidra22@users.noreply.github.com>
2024-05-17 18:09:58 +0300

    Delete tion.js

--- tion.js
+++
@@ -1,6 +0,0 @@
-const soundButton = document.getElementById('soundButton');
-const audio = document.getElementById('audio');
-
-soundButton.addEventListener('click', function() {
-    audio.play();
-});
